82f40ba779c2706ec7926e0f8fcbaa9afa2a36bd
zzn <zzn@qq.com>
2023-04-16 20:10:14 +0800

    hod-fix one commit

--- src/main/java/cn/csxy/gittest2/bean/User.java
+++ src/main/java/cn/csxy/gittest2/bean/User.java
@@ -4,5 +4,6 @@ public class User {
     public static void main(String[] args) {
         System.out.println("git test");
         System.out.println("master test");
+        System.out.println("hod-fix test");
     }
 }
